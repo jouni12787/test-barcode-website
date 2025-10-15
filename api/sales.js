9cc1f519959083dcdd810c5076c4019384481f04
jouni12787 <ajouni913@gmail.com>
2025-10-15 14:35:10 +0300

    Update sales.js

--- api/sales.js
+++ api/sales.js
@@ -42,6 +42,7 @@ export default async function handler(req, res) {
         'X-Token': process.env.SELL_API_TOKEN
       },
       body: JSON.stringify({
+        token: process.env.SELL_API_TOKEN,
         sku: String(sku).trim(),
         employee: String(employee).trim(),
         price: numPrice,
